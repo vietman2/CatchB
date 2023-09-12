8800d608fffcba5c1269fc73bf1961a3003c14b8
vietman2 <vietman0@gmail.com>
2023-09-12 14:55:11 +0900

    remove unnecessary imports

--- CatchB/pages/Home/CoachDetail/CoachDetail.tsx
+++ CatchB/pages/Home/CoachDetail/CoachDetail.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View } from "react-native";
 import { useRoute } from "@react-navigation/native";
 
 import { HomeStackScreenProps } from "../../../containers/navigation";
--- CatchB/pages/Home/CoachDetail/components/Buttons.tsx
+++ CatchB/pages/Home/CoachDetail/components/Buttons.tsx
@@ -9,10 +9,15 @@ export default function Buttons() {
 
   const toggleSwitch = () => setIsLiked((previousState) => !previousState);
 
+  const colors = {
+    true: "#81b0ff",
+    false: "#767577",
+  }
+
   return (
     <View style={styles.buttons}>
       <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        trackColor={colors}
         thumbColor={true ? "#f5dd4b" : "#f4f3f4"}
         ios_backgroundColor="#3e3e3e"
         onValueChange={toggleSwitch}
