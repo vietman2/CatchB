ac40cf44786e6725065561d5c6ae33e511099ff7
vietman2 <vietman0@gmail.com>
2023-11-19 15:10:34 +0900

    test coverage

--- CatchB/src/navigation/tests/HomeStack.test.tsx
+++ CatchB/src/navigation/tests/HomeStack.test.tsx
@@ -1,25 +1,77 @@
-import { render } from "@testing-library/react-native";
+import { act, fireEvent, render } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeContainer from "../HomeStack";
+import CoachDetail from "../../pages/CoachDetail";
+import FacilityDetail from "../../pages/FacilityDetail";
 
+jest.mock("react-native-gesture-handler", () => ({
+  PanGestureHandler: "PanGestureHandler",
+}));
 jest.mock("react-native-vector-icons/Ionicons", () => "Ionicons");
-jest.mock("@react-navigation/stack", () => {
-  return {
-    createStackNavigator: () => ({
-      Navigator: ({ children }: { children: React.ReactNode }) => (
-        <>{children}</>
-      ),
-      Screen: () => null,
-    }),
-  };
-});
-jest.mock("expo-font", () => "expo-font");
+jest.mock("expo-font", () => ({
+  useFonts: jest.fn().mockReturnValue([true, null]),
+}));
+jest.mock(
+  "assets/fonts/KBO_Dia_Gothic_medium.ttf",
+  () => "KBO Dia Gothic Medium Mock"
+);
+jest.mock(
+  "assets/fonts/KBO_Dia_Gothic_bold.ttf",
+  () => "KBO Dia Gothic Bold Mock"
+);
+jest.mock(
+  "assets/fonts/KBO_Dia_Gothic_light.ttf",
+  () => "KBO Dia Gothic Light Mock"
+);
+
 jest.mock("expo-splash-screen", () => ({
   preventAutoHideAsync: jest.fn(),
 }));
 
 describe("<HomeContainer />", () => {
   it("renders correctly", () => {
-    render(<HomeContainer />);
+    render(
+      <NavigationContainer>
+        <HomeContainer />
+      </NavigationContainer>
+    );
   });
+
+  const Stack = createStackNavigator();
+
+  it("navigates to CoachDetail and checks header options", async () => {
+    const component = (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeContainer} />
+          <Stack.Screen name="CoachDetail" component={CoachDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+    const { getByText } = render(component);
+    await act(() => {
+      fireEvent.press(getByText("이대호 코치"));
+    });
+  });
+
+  it("navigates to FacilityDetail and checks header options", async () => {
+    const component = (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeContainer} />
+          <Stack.Screen name="FacilityDetail" component={FacilityDetail} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+
+    const { getByText } = render(component);
+
+    await act(() => {
+      fireEvent.press(getByText("원스타베이스볼 아카데미"));
+    });
+  });
+
 });
