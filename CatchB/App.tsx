d33d3c271f06da7571c92e7d71e04b031692f39a
vietman2 <vietman0@gmail.com>
2023-11-19 17:05:53 +0900

    authslice

--- CatchB/App.tsx
+++ CatchB/App.tsx
@@ -1,14 +1,11 @@
 import { NavigationContainer } from "@react-navigation/native";
-import CodePush from "react-native-code-push";
 
 import TabContainer from "./src/navigation/TabStack";
 
-function App() {
+export default function App() {
   return (
     <NavigationContainer>
       <TabContainer />
     </NavigationContainer>
   );
 }
-
-export default CodePush(App);
--- CatchB/src/tabs/MyPage.tsx
+++ CatchB/src/tabs/MyPage.tsx
@@ -1,9 +1,23 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function MyPage() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>마이페이지 화면</Text>
+    <View style={styles.container}>
+      <View style={styles.top}>
+        <Text style={styles.leftTitle}>Catch B</Text>
+        <Text style={styles.rightTitle}>캐치비</Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, marginTop: 20 },
+  top: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 27,
+  },
+  leftTitle: { fontSize: 20, fontWeight: "bold", color: "green" },
+  rightTitle: { fontSize: 20, fontWeight: "bold", color: "brown" },
+});
