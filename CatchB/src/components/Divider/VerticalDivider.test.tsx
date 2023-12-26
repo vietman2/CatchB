1d86a120b114ec002031f7b43cd78772aa349867
vietman2 <vietman0@gmail.com>
2023-12-26 11:30:51 +0900

    refactor

---
+++ CatchB/src/components/Divider/VerticalDivider.test.tsx
@@ -0,0 +1,9 @@
+import { render } from "@testing-library/react-native";
+
+import VerticalDivider from "./VerticalDivider";
+
+describe("<VerticalDivider />", () => {
+  it("renders correctly", () => {
+    render(<VerticalDivider />);
+  });
+});
