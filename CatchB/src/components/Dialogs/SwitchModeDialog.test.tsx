29bde53b2f7a8d7bf80afb04605c7a65078d2752
vietman2 <vietman0@gmail.com>
2023-12-26 11:31:26 +0900

    switch mode dialog

---
+++ CatchB/src/components/Dialogs/SwitchModeDialog.test.tsx
@@ -0,0 +1,57 @@
+import { fireEvent } from "@testing-library/react-native";
+
+import SwitchModeDialog from "./SwitchModeDialog";
+import { renderWithProviders } from "../../utils/test-utils";
+import { admin } from "../../variables/mvp_dummy_data/user";
+
+describe("SwitchModeDialog", () => {
+  it("renders basic mode correctly, and handles button press", () => {
+    const { getByText } = renderWithProviders(
+      <SwitchModeDialog
+        visible={true}
+        currentMode="basic"
+        user={null}
+        onClose={() => {}}
+        setMode={() => {}}
+      />, {
+        preloadedState: { mode: { mode: "basic" } },
+      }
+    );
+
+    fireEvent.press(getByText("확인"));
+  });
+
+  it("renders pro mode correctly", () => {
+    const { getByText } = renderWithProviders(
+      <SwitchModeDialog
+        visible={true}
+        currentMode="pro"
+        user={null}
+        onClose={() => {}}
+        setMode={() => {}}
+      />,
+      {
+        preloadedState: { mode: { mode: "pro" } },
+      }
+    );
+
+    fireEvent.press(getByText("확인"));
+  });
+
+  it("renders basic mode correctly with user", () => {
+    const { getByText } = renderWithProviders(
+      <SwitchModeDialog
+        visible={true}
+        currentMode="basic"
+        user={admin}
+        onClose={() => {}}
+        setMode={() => {}}
+      />,
+      {
+        preloadedState: { mode: { mode: "basic" } },
+      }
+    );
+    
+    fireEvent.press(getByText("확인"));
+  });
+});
