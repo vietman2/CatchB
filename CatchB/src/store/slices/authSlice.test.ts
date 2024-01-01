30a046af2b9720a47b34f2890a487fae322ba203
vietman2 <vietman0@gmail.com>
2024-01-02 08:34:22 +0900

    secure store to save refresh token

--- CatchB/src/store/slices/authSlice.test.ts
+++ CatchB/src/store/slices/authSlice.test.ts
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import thunk from "redux-thunk";
 
-import authReducer, { login, logout } from "./authSlice";
+import authReducer, { login, logout, setNewToken, setUserProfile } from "./authSlice";
 import { admin } from "../../variables/mvp_dummy_data/user";
 
 // Create a mock store for testing
@@ -18,7 +18,7 @@ describe("authSlice", () => {
   // Test for initial state
   it("should handle initial state", () => {
     expect(authReducer(undefined, { type: "unknown" })).toEqual({
-      token: null,
+      token: "",
       user: null,
     });
   });
@@ -27,9 +27,10 @@ describe("authSlice", () => {
   it("should handle login", async () => {
     const store = createStore();
     const access = "user-token";
+    const refresh = "user-refresh-token";
     const user = admin;
 
-    await store.dispatch(login({access, user}));
+    await store.dispatch(login({access, refresh, user}));
 
     const state = store.getState().auth;
     expect(state.token).toEqual(access);
@@ -38,9 +39,29 @@ describe("authSlice", () => {
   // Test for logout action
   it("should handle logout", async () => {
     const store = createStore();
-    store.dispatch(logout());
+
+    await store.dispatch(logout());
 
     const state = store.getState().auth;
-    expect(state.token).toBeNull();
+    expect(state.token).toEqual("");
+  });
+
+  it("should handle setNewToken", async () => {
+    const store = createStore();
+    const newToken = "new-token";
+    
+    await store.dispatch(setNewToken(newToken));
+
+    const state = store.getState().auth;
+    expect(state.token).toEqual("new-token");
+  });
+
+  it("should handle setUserProfile", async () => {
+    const store = createStore();
+    
+    await store.dispatch(setUserProfile(admin));
+
+    const state = store.getState().auth;
+    expect(state.user).toEqual(admin);
   });
 });
--- CatchB/src/store/slices/authSlice.ts
+++ CatchB/src/store/slices/authSlice.ts
@@ -1,23 +1,47 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import { UserProfile } from "../../variables/types";
+import { save, remove } from "../secure";
 
 const initialState: {
-  token: string | null;
+  token: string;
   user: UserProfile | null;
 } = {
-  token: null,
+  token: "",
   user: null,
 };
 
 export const login = createAsyncThunk(
   "auth/login",
-  async (data: { access: string, user: UserProfile }) => {
+  async (data: { access: string, refresh: string, user: UserProfile }) => {
+    // Save the token to SecureStore
+    await save("refresh_token", data.refresh);
+    await save("uuid", data.user.uuid);
+
+    return data;
+  }
+);
+
+export const setNewToken = createAsyncThunk(
+  "auth/renewToken",
+  async (data: string) => {
+    return data;
+  }
+);
+
+export const setUserProfile = createAsyncThunk(
+  "auth/getUserProfile",
+  async (data: UserProfile ) => {
+
     return data;
   }
 );
 
 export const logout = createAsyncThunk("auth/logout", async () => {
+  // Delete the token from SecureStore
+  await remove("refresh_token");
+  await remove("uuid");
+
   return null;
 });
 
@@ -31,8 +55,14 @@ export const authSlice = createSlice({
       state.user = action.payload.user;
     });
     builder.addCase(logout.fulfilled, (state) => {
-      state.token = null;
+      state.token = "";
       state.user = null;
+    });
+    builder.addCase(setNewToken.fulfilled, (state, action) => {
+      state.token = action.payload;
+    });
+    builder.addCase(setUserProfile.fulfilled, (state, action) => {
+      state.user = action.payload;
     });
   }
 });
