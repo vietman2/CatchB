8d861dad0d0634503090647a5d14473c56e57bab
vietman2 <vietman0@gmail.com>
2024-01-09 15:46:22 +0900

    coupon register api integration

---
+++ CatchB/src/containers/MyPage/Coupon/CouponRegister.tsx
@@ -0,0 +1,117 @@
+import { useState } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  View,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Button, TextInput, Text } from "react-native-paper";
+import { useDispatch, useSelector } from "react-redux";
+
+import { registerCoupon, checkStatus } from "../../../services/coupon";
+import { AppDispatch, RootState } from "../../../store/store";
+import { themeColors } from "../../../variables/colors";
+
+export default function CouponRegister() {
+  const [code, setCode] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+  const access = useSelector((state: RootState) => state.auth.token);
+  const dispatch = useDispatch<AppDispatch>();
+
+  const handleCodeInput = (text: string) => {
+    let cleaned = text.replace(/[^a-zA-Z0-9]/g, "");
+    let chunks = cleaned.match(/.{1,4}/g) || [];
+    let formatted = chunks.join("-");
+
+    formatted = formatted.toUpperCase();
+    formatted = formatted.substring(0, 19);
+
+    setCode(formatted);
+    setError("");
+  };
+
+  const handleRegister = async () => {
+    setLoading(true);
+    const response = await registerCoupon(code, access);
+
+    if (response.status === 202) {
+      setError("");
+      //setCode("");
+
+      const taskId = response.data.task_id;
+
+      const taskResponse = await checkStatus(taskId, access);
+
+      setInterval(async () => {
+        if (taskResponse.status === 200) {
+          setLoading(false);
+        } else {
+          setError("에러");
+        }
+      }, 2000);
+    } else {
+      setError(response.data.message);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <View style={{ flex: 1 }}>
+          <TextInput
+            style={styles.textInput}
+            outlineStyle={{ borderColor: "black" }}
+            cursorColor="black"
+            value={code}
+            onChangeText={(text) => handleCodeInput(text)}
+            mode="outlined"
+            showSoftInputOnFocus={true}
+            placeholder="쿠폰 코드를 입력해주세요"
+            testID="coupon-register-text-input"
+          />
+          <View style={{ alignItems: "flex-end" }}>
+            {error ? <Text style={styles.errorText}>{error}</Text> : null}
+          </View>
+
+          <Button
+            onPress={handleRegister}
+            mode="contained-tonal"
+            style={styles.button}
+            labelStyle={styles.buttonText}
+            loading={loading}
+          >
+            {loading ? null : "쿠폰 등록"}
+          </Button>
+        </View>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themeColors.primaryContainer,
+    paddingHorizontal: 20,
+  },
+  textInput: {
+    marginTop: 20,
+    backgroundColor: themeColors.primaryContainer,
+    alignItems: "center",
+  },
+  button: {
+    marginTop: 20,
+    backgroundColor: themeColors.primary,
+  },
+  buttonText: {
+    color: themeColors.primaryContainer,
+    fontSize: 20,
+  },
+  errorText: {
+    color: themeColors.error,
+    marginTop: 5,
+  },
+});
