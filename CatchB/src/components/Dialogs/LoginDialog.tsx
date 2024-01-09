4429bd0b3d3a6f68b48cd577cbfc52e466566089
vietman2 <vietman0@gmail.com>
2024-01-09 15:46:39 +0900

    spacing

--- CatchB/src/components/Dialogs/LoginDialog.tsx
+++ CatchB/src/components/Dialogs/LoginDialog.tsx
@@ -1,24 +1,29 @@
 import { Dialog, Portal, Text, Button } from "react-native-paper";
 
 interface Props {
-    visible: boolean;
-    title: string;
-    contents: string;
-    onClose: () => void;
+  visible: boolean;
+  title: string;
+  contents: string;
+  onClose: () => void;
 }
 
-export default function LoginDialog({ visible, title, contents, onClose }: Props) {
-    return (
-      <Portal>
-        <Dialog visible={visible}>
-          <Dialog.Title>{title}</Dialog.Title>
-          <Dialog.Content>
-            <Text>{contents}</Text>
-          </Dialog.Content>
-          <Dialog.Actions>
-            <Button onPress={onClose}>확인</Button>
-          </Dialog.Actions>
-        </Dialog>
-      </Portal>
-    );
+export default function LoginDialog({
+  visible,
+  title,
+  contents,
+  onClose,
+}: Props) {
+  return (
+    <Portal>
+      <Dialog visible={visible}>
+        <Dialog.Title>{title}</Dialog.Title>
+        <Dialog.Content>
+          <Text>{contents}</Text>
+        </Dialog.Content>
+        <Dialog.Actions>
+          <Button onPress={onClose}>확인</Button>
+        </Dialog.Actions>
+      </Dialog>
+    </Portal>
+  );
 }
