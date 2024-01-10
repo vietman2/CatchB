8eb448a963bfe44d0bb5da1430cfc6151fd5c9ee
vietman2 <vietman0@gmail.com>
2024-01-10 10:14:02 +0900

    apply coupon list api and state

--- CatchB/src/containers/MyPage/Coupon/CouponList.tsx
+++ CatchB/src/containers/MyPage/Coupon/CouponList.tsx
@@ -1,65 +1,24 @@
+import { useEffect, useState } from "react";
 import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
-import { Icon, Text } from "react-native-paper";
-import { useSelector } from "react-redux";
+import { ActivityIndicator, Text } from "react-native-paper";
+import { useSelector, useDispatch } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
-import { LinearGradient } from "expo-linear-gradient";
 
+import NoCoupon from "../../../components/Coupons/NoCoupon";
+import MyCoupon from "../../../components/Coupons/MyCoupon";
 import { MyPageStackScreenProps } from "../../../variables/navigation";
 import { themeColors } from "../../../variables/colors";
-import { Coupon as CouponType } from "../../../variables/types";
-import { RootState } from "../../../store/store";
+import { getCouponList } from "../../../services/coupon";
+import { setCouponListState } from "../../../store/slices/couponSlice";
+import { AppDispatch, RootState } from "../../../store/store";
 
 export default function CouponList() {
-  const navigation = useNavigation<MyPageStackScreenProps<"CouponList">["navigation"]>();
+  const [loading, setLoading] = useState(true);
+  const navigation =
+    useNavigation<MyPageStackScreenProps<"CouponList">["navigation"]>();
+  const token = useSelector((state: RootState) => state.auth.token);
   const coupons = useSelector((state: RootState) => state.coupon.coupons);
-
-  const NoCoupon = () => {
-    return (
-      <View style={styles.noCoupon}>
-        <View
-          style={{
-            backgroundColor: "gray",
-            width: 100,
-            height: 100,
-            marginBottom: 20,
-          }}
-        />
-        <Text variant="titleLarge" style={{ fontWeight: "bold" }}>
-          보유한 쿠폰이 없습니다.
-        </Text>
-      </View>
-    );
-  };
-
-  const Coupon = ({ coupon }: { coupon: CouponType }) => {
-    return (
-      <View style={styles.coupon}>
-        <LinearGradient
-          colors={["white", "silver"]}
-          style={styles.gradient}
-          start={{ x: 0, y: 0 }}
-          end={{ x: 1, y: 1 }}
-          locations={[ 0.4, 0.9]}
-        >
-          <View style={styles.couponInfo}>
-            <Text
-              variant="headlineMedium"
-              style={{ fontFamily: "Catch B ExtraBold", marginBottom: 5 }}
-            >
-              {coupon.coupon_class.coupon_name}
-            </Text>
-            <Text variant="bodyLarge">
-              {coupon.coupon_class.coupon_description}
-            </Text>
-            <Text variant="bodySmall">만료기한 {coupon.valid_until}</Text>
-          </View>
-          <View style={styles.download}>
-            <Icon source="tray-arrow-down" size={32} />
-          </View>
-        </LinearGradient>
-      </View>
-    );
-  };
+  const dispatch = useDispatch<AppDispatch>();
 
   const getNumberCoupons = () => {
     return coupons.length;
@@ -69,6 +28,15 @@ export default function CouponList() {
     navigation.navigate("CouponRegister");
   };
 
+  useEffect(() => {
+    const getCoupons = async () => {
+      const response = await getCouponList(token);
+      dispatch(setCouponListState(response.data));
+      setLoading(false);
+    };
+    getCoupons();
+  }, []);
+
   return (
     <View style={styles.container}>
       <View style={styles.topBar}>
@@ -77,12 +45,14 @@ export default function CouponList() {
           <Text variant="titleMedium">+ 쿠폰등록</Text>
         </TouchableOpacity>
       </View>
-      {coupons.length === 0 ? (
+      {loading ? (
+        <ActivityIndicator size={"large"} color={themeColors.primary} style={{ flex: 1, marginBottom: 30 }} />
+      ) : coupons.length === 0 ? (
         <NoCoupon />
       ) : (
         <ScrollView>
           {coupons.map((coupon, index) => {
-            return <Coupon key={index} coupon={coupon} />;
+            return <MyCoupon key={index} coupon={coupon} />;
           })}
         </ScrollView>
       )}
@@ -101,32 +71,5 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginTop: 20,
     marginBottom: 10,
-  },
-  noCoupon: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  coupon: {
-    borderRadius: 10,
-    borderWidth: 1,
-    backgroundColor: "silver",
-    flexDirection: "row",
-    marginHorizontal: 20,
-    marginVertical: 10,
-  },
-  gradient: {
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    paddingHorizontal: 25,
-    borderRadius: 10,
-  },
-  couponInfo: {
-    flex: 6,
-    marginRight: 50,
-    marginVertical: 20,
-  },
-  download: {
-    flex: 1,
   },
 });
