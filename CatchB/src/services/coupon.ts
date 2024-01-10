dfbb8f0c9f720131fbc9a141b91d96d0c8d2c0a6
vietman2 <vietman0@gmail.com>
2024-01-10 10:13:43 +0900

    add couponlist api and state

--- CatchB/src/services/coupon.ts
+++ CatchB/src/services/coupon.ts
@@ -2,6 +2,34 @@ import axios from "axios";
 
 import { API_LOCAL_URL } from "./apiConfig";
 
+export async function getCouponList(access: string) {
+  const url = `${API_LOCAL_URL}/api/users/coupons/`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+    });
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (err) {
+    if (err.response) {
+      return {
+        status: err.response.status,
+        data: err.response.data,
+      };
+    }
+    return {
+      status: 500,
+      data: "Server Error",
+    };
+  }
+}
+
 export async function registerCoupon(coupon_code: string, access: string) {
   const url = `${API_LOCAL_URL}/api/users/coupons/register/`;
 
--- CatchB/src/store/slices/couponSlice.ts
+++ CatchB/src/store/slices/couponSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 import { Coupon } from "../../variables/types";
 
@@ -10,11 +10,22 @@ const initialState: {
   selectedCoupon: null,
 };
 
+export const setCouponListState = createAsyncThunk(
+  "coupon/setCouponListState",
+  async (data: Coupon[]) => {
+    return data;
+  }
+);
+
 export const couponSlice = createSlice({
-    name: "coupon",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {},
+  name: "coupon",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(setCouponListState.fulfilled, (state, action) => {
+      state.coupons = action.payload;
+    });
+  },
 });
 
 export default couponSlice.reducer;
