e6470a43f5a1e7305a8f839d5c3fc465c499f417
vietman2 <vietman0@gmail.com>
2024-01-11 10:19:07 +0900

    points api

---
+++ CatchB/src/services/point.ts
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+import { API_LOCAL_URL } from "./apiConfig";
+
+export async function getPointsList(access: string, uuid: string) {
+  const url = `${API_LOCAL_URL}/api/users/points/`;
+
+  try {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${access}`,
+      },
+      params: {
+        uuid,
+      },
+    });
+
+    return {
+      status: response.status,
+      data: response.data,
+    };
+  } catch (err) {
+    if (err.response) {
+      return {
+        status: err.response.status,
+        data: err.response.data,
+      };
+    }
+    return {
+      status: 500,
+      data: "Server Error",
+    };
+  }
+}
