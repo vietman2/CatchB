eda67ea84651d3c2352a1252a2b4e43a810a4811
vietman2 <vietman0@gmail.com>
2024-01-11 14:57:59 +0900

    create coach reg and fac reg pages

---
+++ CatchB/src/components/Checkboxes/MultiCheck.tsx
@@ -0,0 +1,52 @@
+import { View } from "react-native";
+import { Chip } from "react-native-paper";
+import { themeColors } from "../../variables/colors";
+
+interface Props {
+  options: string[];
+  selected: string[];
+  setSelected: (selected: string[]) => void;
+}
+
+export default function MultiCheck({ options, selected, setSelected }: Props) {
+
+  const isSelected = (option: string) => {
+    return selected.includes(option);
+  };
+
+  const toggleSelected = (option: string) => {
+    if (selected.includes(option)) {
+      setSelected(selected.filter((item) => item !== option));
+    } else {
+      setSelected([...selected, option]);
+    }
+  };
+
+  return (
+    <>
+      <View style={{ flexDirection: "row" }}>
+        {options.map((option, index) => (
+          <Chip
+            key={index}
+            style={{
+              marginRight: 10,
+              marginBottom: 10,
+              backgroundColor: isSelected(option)
+                ? themeColors.primary
+                : themeColors.tertiary,
+            }}
+            mode="flat"
+            showSelectedCheck
+            showSelectedOverlay
+            selectedColor="white"
+            compact
+            selected={isSelected(option)}
+            onPress={() => toggleSelected(option)}
+          >
+            {option}
+          </Chip>
+        ))}
+      </View>
+    </>
+  );
+}
---
+++ CatchB/src/components/Checkboxes/SingleCheck.tsx
@@ -0,0 +1,45 @@
+import { View } from "react-native";
+import { Chip } from "react-native-paper";
+import { themeColors } from "../../variables/colors";
+
+interface Props {
+  options: string[];
+  selected: string;
+  setSelected: (selected: string) => void;
+}
+
+export default function SingleCheck({ options, selected, setSelected }: Props) {
+
+  const isSelected = (option: string) => {
+    return selected === option;
+  };
+  const toggleSelected = (option: string) => {
+    if (selected === option) {
+      setSelected("");
+    } else {
+      setSelected(option);
+    }
+  };
+
+  return (
+    <>
+      <View style={{ flexDirection: "row" }}>
+        {options.map((option, index) => (
+          <Chip
+            key={index}
+            style={{ marginRight: 10, marginBottom: 10, backgroundColor: isSelected(option) ? themeColors.primary : themeColors.tertiary }}
+            mode="flat"
+            showSelectedCheck
+            showSelectedOverlay
+            selectedColor="white"
+            compact
+            selected={isSelected(option)}
+            onPress={() => toggleSelected(option)}
+          >
+            {option}
+          </Chip>
+        ))}
+      </View>
+    </>
+  );
+}
