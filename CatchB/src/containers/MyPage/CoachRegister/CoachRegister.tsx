2b94533875a7e268be928d3f4d29652ded1b031d
vietman2 <vietman0@gmail.com>
2024-01-16 06:07:33 +0900

    add zone search component

--- CatchB/src/containers/MyPage/CoachRegister/CoachRegister.tsx
+++ CatchB/src/containers/MyPage/CoachRegister/CoachRegister.tsx
@@ -8,8 +8,10 @@ import SingleCheck from "../../../components/Checkboxes/SingleCheck";
 import MultiCheck from "../../../components/Checkboxes/MultiCheck";
 import { themeColors } from "../../../variables/colors";
 import { RootState } from "../../../store/store";
+import AreaPicker from "../../../components/Dialogs/AreaPicker";
 
 export default function CoachRegister() {
+  const [visible, setVisible] = useState<boolean>(false);
   const [selectedGender, setSelectedGender] = useState<string>("");
   const [selectedAreas, setSelectedAreas] = useState<string[]>([]);
   const user = useSelector((state: RootState) => state.auth.user);
@@ -22,64 +24,70 @@ export default function CoachRegister() {
   };
 
   return (
-    <ScrollView style={styles.container}>
-      <Text variant="titleLarge" style={styles.title}>
-        기본 정보
-      </Text>
-      <Text variant="titleSmall" style={styles.subtitle}>
-        이름 *
-      </Text>
-      <TextInput
-        mode="outlined"
-        value={user.full_name}
-        disabled
-        dense
-        textColor="black"
-        style={styles.filled}
-      />
-      <Text variant="titleSmall" style={styles.subtitle}>
-        전화번호 *
-      </Text>
-      <TextInput
-        mode="outlined"
-        value={user.phone_number}
-        disabled
-        dense
-        textColor="black"
-        style={styles.filled}
-      />
-      <Text variant="titleSmall" style={styles.subtitle}>
-        성별 *
-      </Text>
-      <SingleCheck
-        options={["남자", "여자"]}
-        selected={selectedGender}
-        setSelected={setSelectedGender}
-      />
-      <Text variant="titleSmall" style={styles.subtitle}>
-        활동 지역 (최대 5개 지역까지 선택 가능) *
-      </Text>
-      <MultiCheck
-        options={["서울", "경기", "인천"]}
-        selected={selectedAreas}
-        setSelected={setSelectedAreas}
-      />
-      <Text variant="titleSmall" style={styles.subtitle}>
-        경력 *
-      </Text>
-      <Text variant="titleSmall" style={styles.subtitle}>
-        코치 자격증 업로드 *
-      </Text>
-      <Button
-        mode="contained-tonal"
-        icon="upload"
-        buttonColor={themeColors.primary}
-        textColor={themeColors.onPrimary}
-        onPress={handleUpload}
-      >
-        업로드
-      </Button>
-    </ScrollView>
+    <>
+      <ScrollView style={styles.container}>
+        <Text variant="titleLarge" style={styles.title}>
+          기본 정보
+        </Text>
+        <Text variant="titleSmall" style={styles.subtitle}>
+          이름 *
+        </Text>
+        <TextInput
+          mode="outlined"
+          value={user.full_name}
+          disabled
+          dense
+          textColor="black"
+          style={styles.filled}
+        />
+        <Text variant="titleSmall" style={styles.subtitle}>
+          전화번호 *
+        </Text>
+        <TextInput
+          mode="outlined"
+          value={user.phone_number}
+          disabled
+          dense
+          textColor="black"
+          style={styles.filled}
+        />
+        <Text variant="titleSmall" style={styles.subtitle}>
+          성별 *
+        </Text>
+        <SingleCheck
+          options={["남자", "여자"]}
+          selected={selectedGender}
+          setSelected={setSelectedGender}
+        />
+        <Text variant="titleSmall" style={styles.subtitle}>
+          활동 지역 (최대 5개 지역까지 선택 가능) *
+        </Text>
+        <MultiCheck
+          options={["서울", "경기", "인천"]}
+          selected={selectedAreas}
+          setSelected={setSelectedAreas}
+        />
+        <Text variant="titleSmall" style={styles.subtitle}>
+          경력 *
+        </Text>
+        <Text variant="titleSmall" style={styles.subtitle}>
+          코치 자격증 업로드 *
+        </Text>
+        <Button
+          mode="contained-tonal"
+          icon="upload"
+          buttonColor={themeColors.primary}
+          textColor={themeColors.onPrimary}
+          onPress={handleUpload}
+        >
+          업로드
+        </Button>
+        <Button mode="contained-tonal" onPress={() => setVisible(true)}>
+          Example
+        </Button>
+      </ScrollView>
+      <AreaPicker visible={visible} onDismiss={() => setVisible(false)} />
+    </>
   );
 }
 
