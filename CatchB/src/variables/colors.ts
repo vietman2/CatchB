b75ed0b7039fba2358f65a1042c27ea16968a6d8
vietman2 <vietman0@gmail.com>
2024-01-16 06:04:34 +0900

    change onSecondaryContainer color

--- CatchB/src/variables/colors.ts
+++ CatchB/src/variables/colors.ts
@@ -4,7 +4,7 @@ export const themeColors = {
   primaryContainer: "#fff",
   secondary: "brown",
   secondaryContainer: "rgba(0, 0, 0, 0.25)",
-  onSecondaryContainer: "blue", // 탭 아이콘 색상, 칩 오른쪽 아이콘 색상
+  onSecondaryContainer: "yellow", // 탭 아이콘 색상, 칩 오른쪽 아이콘 색상
   tertiary: "gray",
   tertiaryContainer: "rgba(64, 196, 20, 0.5)",
   error: "red",
