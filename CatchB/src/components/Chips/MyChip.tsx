c889d8879d2d7610184972c835a0105f7a2e1b33
vietman2 <vietman0@gmail.com>
2024-02-05 16:12:46 +0900

    chip component for selections

---
+++ CatchB/src/components/Chips/MyChip.tsx
@@ -0,0 +1,44 @@
+import { View, StyleSheet } from "react-native";
+import { Icon, Text } from "react-native-paper";
+
+interface Props {
+  selected: boolean;
+  label: string;
+  custom?: boolean;
+}
+
+export default function MyChip({ selected, label, custom }: Props) {
+  custom = custom ? custom : false;
+
+  return (
+    <View style={styles.container}>
+      <Text
+        variant="bodyLarge"
+        style={[styles.text, { color: selected ? "white" : "black" }]}
+      >
+        {label}
+      </Text>
+      {selected ? (
+        <Icon source="check" color="white" size={20} />
+      ) : (
+        <Icon source="check" color="transparent" size={20} />
+      )}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingVertical: 5,
+    paddingLeft: 10,
+    paddingRight: 6,
+  },
+  text: {
+    fontWeight: "600",
+    textAlign: "center",
+    paddingLeft: 5,
+  },
+});
