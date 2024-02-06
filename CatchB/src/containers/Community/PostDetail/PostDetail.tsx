3cbe58f0e9750dfda6ca3899e8faaa3470c79872
vietman2 <vietman0@gmail.com>
2024-02-06 13:39:37 +0900

    use state to handle posts

--- CatchB/src/containers/Community/PostDetail/PostDetail.tsx
+++ CatchB/src/containers/Community/PostDetail/PostDetail.tsx
@@ -1,10 +1,44 @@
-import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { StyleSheet, View } from "react-native";
+import { Chip, Text } from "react-native-paper";
+import { useSelector } from "react-redux";
+
+import { RootState } from "../../../store/store";
+import { themeColors } from "../../../variables/colors";
 
 export default function PostDetail() {
+  const post = useSelector((state: RootState) => state.community.selectedPost)
+
   return (
-    <View>
-      <Text>PostDetail</Text>
+    <View style={styles.container}>
+      <View style={styles.topBar}>
+        <Chip icon="account" style={{ marginTop: 10, marginRight: 10 }} compact>
+          {post.author_name}
+        </Chip>
+        <Text variant="headlineMedium" style={styles.title}>
+          {post.title}
+        </Text>
+      </View>
+      <Text variant="titleMedium" style={styles.body}>
+        {post.body}
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: themeColors.primaryContainer,
+    paddingHorizontal: 20,
+  },
+  topBar: {
+    flexDirection: "row",
+  },
+  title: {
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  body: {
+    marginTop: 10,
+  },
+});
