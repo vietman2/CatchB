c06a73e3e9237ba019c45c253bf5a5d3f63a178f
vietman2 <vietman0@gmail.com>
2024-02-09 13:12:08 +0900

    add components to normal home

--- CatchB/src/containers/Home/Main/HomeMain.test.tsx
+++ CatchB/src/containers/Home/Main/HomeMain.test.tsx
@@ -1,41 +1,37 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { fireEvent } from "@testing-library/react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 
 import HomeMain from "./HomeMain";
 import { renderWithProviders } from "../../../utils/test-utils";
 import { admin } from "../../../variables/mvp_dummy_data/user";
 
+jest.mock("react-native-gesture-handler", () => ({
+  PanGestureHandler: "PanGestureHandler",
+}));
 jest.mock("react-native-paper", () => {
   const Provider = jest.requireActual("react-native-paper").PaperProvider;
   const { View, TouchableOpacity, Text } = jest.requireActual("react-native");
 
   return {
     PaperProvider: Provider,
-    Banner: ({ visible, actions, icon, children }: any) => {
-      if (visible) {
-        return (
-          <View>
-            {icon}
-            {children}
-            {actions.map(({ label, onPress }: any) => (
-              <TouchableOpacity
-                key={label}
-                onPress={onPress}
-                accessibilityLabel={label}
-              />
-            ))}
-          </View>
-        );
-      } else {
-        return null;
-      }
-    },
     Text: "Text",
     Icon: "Icon",
     Button: ({ onPress, children }: any) => (
-      <TouchableOpacity onPress={onPress} accessibilityLabel="버튼">
+      <TouchableOpacity onPress={onPress}>
         <Text>{children}</Text>
       </TouchableOpacity>
+    ),
+    Surface: ({ children }: any) => <View>{children}</View>,
+    Chip: ({ onPress, children }: any) => (
+      <TouchableOpacity onPress={onPress}>
+        <Text>{children}</Text>
+      </TouchableOpacity>
+    ),
+    Banner: ({ children }: any) => (
+      <View>
+        <Text>{children}</Text>
+      </View>
     ),
   };
 });
@@ -44,21 +40,23 @@ jest.mock("../../../components/Buttons/SurfaceButton", () => "SurfaceButton");
 jest.mock("../../../components/Chips/Chips", () => ({
   NotificationChip: () => "NotificationChip",
 }));
-jest.mock("../../../components/Chips/Chips", () => ({
-  NotificationChip: () => "NotificationChip",
-}));
 jest.mock("../../../components/Cards/SimpleCard", () => "SimpleCard");
 jest.mock("../../../components/Cards/CardBox", () => "CardBox");
 
-const renderWithMode = (mode: "basic" | "pro") => {
-  return renderWithProviders(<HomeMain />, {
-    preloadedState: { general: { mode, location: null } },
-  });
+const Stack = createStackNavigator();
+
+const components = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="HomeMain" component={HomeMain} />
+      </Stack.Navigator>
+    </NavigationContainer>);
 };
 
 describe("<NormalHome />", () => {
   it("renders with user", () => {
-    renderWithProviders(<HomeMain />, {
+    renderWithProviders(components(), {
       preloadedState: {
         general: { mode: "basic", location: null },
         auth: { user: admin, token: "" },
@@ -67,7 +65,7 @@ describe("<NormalHome />", () => {
   });
 
   it("renders without user", () => {
-    renderWithProviders(<HomeMain />, {
+    renderWithProviders(components(), {
       preloadedState: {
         general: { mode: "basic", location: null },
         auth: { user: null, token: "" },
@@ -78,13 +76,11 @@ describe("<NormalHome />", () => {
 
 describe("<ProHome />", () => {
   it("renders correctly", () => {
-    renderWithMode("pro");
-  });
-
-  it("handles banner press", () => {
-    const { getByTestId, getByLabelText } = renderWithMode("pro");
-
-    fireEvent.press(getByLabelText("닫기"));
-    fireEvent.press(getByTestId("banner"));
+    renderWithProviders(components(), {
+      preloadedState: {
+        general: { mode: "pro", location: null },
+        auth: { user: admin, token: "" },
+      },
+    });
   });
 });
