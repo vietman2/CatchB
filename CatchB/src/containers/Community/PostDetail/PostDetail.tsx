f0e9d3e9ec33e36a2cc987a8f70e828d3947d96e
vietman2 <vietman0@gmail.com>
2024-02-13 14:28:06 +0900

    add comments to post detail

--- CatchB/src/containers/Community/PostDetail/PostDetail.tsx
+++ CatchB/src/containers/Community/PostDetail/PostDetail.tsx
@@ -1,27 +1,47 @@
-import { StyleSheet, View } from "react-native";
-import { Chip, Text } from "react-native-paper";
+import { useRef, useMemo } from "react";
+import { ScrollView, StyleSheet, View } from "react-native";
+import { Divider, Text } from "react-native-paper";
 import { useSelector } from "react-redux";
+import BottomSheet from "@gorhom/bottom-sheet";
 
+import PostTagChip from "../../../components/Chips/PostTagChip";
+import PostAuthorProfile from "../../../components/Profile/PostAuthorProfile";
 import { RootState } from "../../../store/store";
 import { themeColors } from "../../../variables/colors";
 
 export default function PostDetail() {
-  const post = useSelector((state: RootState) => state.community.selectedPost)
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["10%", "50%", "100%"], []);
+  const post = useSelector((state: RootState) => state.community.selectedPost);
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topBar}>
-        <Chip icon="account" style={{ marginTop: 10, marginRight: 10 }} compact>
-          {post.author_name}
-        </Chip>
-        <Text variant="headlineMedium" style={styles.title}>
+    <>
+      <ScrollView style={styles.container}>
+        <View style={styles.tags}>
+          <PostTagChip label="일반" />
+        </View>
+        <Text variant="headlineSmall" style={styles.title}>
           {post.title}
         </Text>
-      </View>
-      <Text variant="titleMedium" style={styles.body}>
-        {post.body}
-      </Text>
-    </View>
+        <PostAuthorProfile post={post} />
+        <Divider />
+        <Text variant="titleMedium" style={styles.body}>
+          {post.body}
+        </Text>
+        <Divider />
+        <View style={styles.interactions}>
+          <Text variant="titleSmall">공감하기</Text>
+          <Text variant="titleSmall">신고하기</Text>
+        </View>
+        <Divider />
+      </ScrollView>
+      <BottomSheet ref={bottomSheetRef} index={0} snapPoints={snapPoints} backgroundStyle={{backgroundColor: "rgb(250, 250, 250)"}}>
+        <View style={styles.comments}>
+          <Text variant="titleMedium" style={styles.commentTitle}>댓글 {post.num_comments}</Text>
+          <Divider />
+        </View>
+      </BottomSheet>
+    </>
   );
 }
 
@@ -31,14 +51,30 @@ const styles = StyleSheet.create({
     backgroundColor: themeColors.primaryContainer,
     paddingHorizontal: 20,
   },
-  topBar: {
+  tags: {
     flexDirection: "row",
+    flexWrap: "wrap",
+    marginTop: 10,
   },
   title: {
+    flexWrap: "wrap",
     fontWeight: "bold",
     marginTop: 10,
   },
   body: {
+    marginVertical: 10,
+  },
+  interactions: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     marginTop: 10,
+    marginBottom: 80,
+  },
+  comments: {
+    paddingHorizontal: 20,
+    marginVertical: 10,
+  },
+  commentTitle: {
+    marginBottom: 10,
   },
 });
