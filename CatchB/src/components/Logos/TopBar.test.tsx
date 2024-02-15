2d94274eabb953211eee69e2aac6aa7087b14151
vietman2 <vietman0@gmail.com>
2024-02-15 16:19:40 +0900

    remove right title

--- CatchB/src/components/Logos/TopBar.test.tsx
+++ CatchB/src/components/Logos/TopBar.test.tsx
@@ -1,19 +1,12 @@
 import { render } from "@testing-library/react-native";
 
-import { leftTitle, rightTitle } from "./TopBar";
+import { leftTitle } from "./TopBar";
 
 describe("TopBar", () => {
   describe("leftTitle", () => {
     it("renders correctly", () => {
       const { getByText } = render(leftTitle());
       expect(getByText("Catch B")).toBeTruthy();
-    });
-  });
-
-  describe("rightTitle", () => {
-    it("renders correctly", () => {
-      const { getByText } = render(rightTitle());
-      expect(getByText("캐치비")).toBeTruthy();
     });
   });
 });
