3e41f43717549725a5af694215b87ac409f911f3
vietman2 <vietman0@gmail.com>
2024-02-19 17:45:26 +0900

    add remove image pressable

--- CatchB/src/components/Images/ImagePreview.tsx
+++ CatchB/src/components/Images/ImagePreview.tsx
@@ -1,10 +1,12 @@
-import { View, Image } from "react-native";
+import { View, ImageBackground, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-paper";
 
 interface Props {
   uri: string;
+  removeImage: () => void;
 }
 
-export default function ImagePreview({ uri }: Readonly<Props>) {
+export default function ImagePreview({ uri, removeImage }: Readonly<Props>) {
   return (
     <View
       style={{
@@ -16,10 +18,23 @@ export default function ImagePreview({ uri }: Readonly<Props>) {
         alignItems: "center",
       }}
     >
-      <Image
+      <ImageBackground
         source={{ uri }}
         style={{ width: 120, height: 120, borderRadius: 8 }}
-      />
+      >
+        <TouchableOpacity
+          onPress={removeImage}
+          style={{
+            position: "absolute",
+            top: 0,
+            right: 0,
+            borderRadius: 15,
+            backgroundColor: "rgba(255, 255, 255, 0.25)",
+          }}
+        >
+          <Icon source="minus" size={20} color="red" />
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 }
