111ef6838f56f0bb789b916abbf88793bd0fcdfa
vietman2 <vietman0@gmail.com>
2024-02-23 12:45:04 +0900

    refactor containers

---
+++ CatchB/src/containers/index.tsx
@@ -0,0 +1,17 @@
+import CommunityContainer from "./Community/CommunityStack";
+import HistoryContainer from "./History/HistoryStack";
+import HomeContainer from "./Home/HomeStack";
+import MyPageContainer from "./MyPage/MyPageStack";
+import MyStoreContainer from "./MyStore/MyStoreStack";
+import NearbyContainer from "./Nearby/NearbyStack";
+import PromotionContainer from "./Promotion/PromotionStack";
+
+export {
+  HomeContainer,
+  NearbyContainer,
+  CommunityContainer,
+  HistoryContainer,
+  MyPageContainer,
+  MyStoreContainer,
+  PromotionContainer,
+};
