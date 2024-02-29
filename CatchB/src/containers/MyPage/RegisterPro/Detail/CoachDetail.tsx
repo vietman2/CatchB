26b63f7395f9c61f3f416a40d8febc397d105a1a
vietman2 <vietman0@gmail.com>
2024-02-29 15:49:08 +0900

    work coach detail

--- CatchB/src/containers/MyPage/RegisterPro/Detail/CoachDetail.tsx
+++ CatchB/src/containers/MyPage/RegisterPro/Detail/CoachDetail.tsx
@@ -1,104 +1,210 @@
-import { useState } from "react";
-import { ScrollView, StyleSheet } from "react-native";
-import { Button, Divider, Text, TextInput } from "react-native-paper";
+import { useEffect, useMemo, useRef, useState } from "react";
+import { ScrollView, StyleSheet, TouchableOpacity, View } from "react-native";
+import {
+  Button,
+  Chip,
+  Divider,
+  Snackbar,
+  Text,
+  TextInput,
+} from "react-native-paper";
+import { ImagePickerAsset } from "expo-image-picker";
+import BottomSheet from "@gorhom/bottom-sheet";
 
+import { partChoices, levelChoices } from "./options";
 import { MainTitle, SubTitle } from "../fragments";
-import { Selector } from "../../../../components/Selectors";
+import { ImagePicker } from "../../../../components/Pickers";
+import { RegionSelector, Selector } from "../../../../components/Selectors";
+import { getRegionsList } from "../../../../services/facility/facility";
 import { themeColors } from "../../../../variables/colors";
+import { Sido, Sigungu } from "../../../../variables/types/products";
 
 interface Props {
   onFinish: () => void;
 }
 
-export default function CoachDetail({ onFinish }: Readonly<Props>) {
-  const [selected, setSelected] = useState<string[]>(["왕초보/기초"]);
-  const [curriculum, setCurriculum] = useState<string>("");
-  const [career, setCareer] = useState<string>("");
-  // time
-  //const [weekdayStart, setWeekdayStart] = useState<string>("");
-  //const [weekdayEnd, setWeekdayEnd] = useState<string>("");
-  //const [saturdayStart, setSaturdayStart] = useState<string>("");
-  //const [saturdayEnd, setSaturdayEnd] = useState<string>("");
-  //const [sundayStart, setSundayStart] = useState<string>("");
-  //const [sundayEnd, setSundayEnd] = useState<string>("");
+type Data = {
+  sido: Sido[];
+  sigungu: Record<string, Sigungu[]>;
+};
 
-  const choices = [
+export default function CoachDetail({ onFinish }: Readonly<Props>) {
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ["93%"], []);
+  const [selectedParts, setSelectedParts] = useState<string[]>(["투구"]);
+  const [selectedLevels, setSelectedLevels] = useState<string[]>([
     "왕초보/기초",
-    "주니어/아동",
-    "중고등 선수",
-    "사회인 야구",
-    "스윙 교정",
-    "투구 교정",
-    "그룹 레슨",
-    "기타",
-  ];
+  ]);
+  const [visible, setVisible] = useState<boolean>(false);
+  const [intro, setIntro] = useState<string>("");
+  const [medias, setMedias] = useState<ImagePickerAsset[]>([]);
+  const [selectedSido, setSelectedSido] = useState<string>("서울");
+  const [selectedRegions, setSelectedRegions] = useState<Sigungu[]>([]);
+  const [data, setData] = useState<Data>();
 
   const handleSubmitSuccess = () => {
     onFinish();
   };
+
   /*
   const handleSubmit = () => {
     // TODO: API 연동
   };*/
 
+  useEffect(() => {
+    const getRegions = async () => {
+      const response = await getRegionsList();
+
+      if (response.status === 200) {
+        setData(response.data);
+      }
+    };
+
+    getRegions();
+  }, []);
+
+  const removeSelected = (selected: Sigungu) => {
+    setSelectedRegions(selectedRegions.filter((region) => region !== selected));
+  };
+
   return (
-    <ScrollView
-      style={styles.container}
-      automaticallyAdjustKeyboardInsets
-      keyboardDismissMode="on-drag"
-    >
-      <MainTitle
-        text="상세 정보"
-        sub="상세 정보를 다 입력하면 문의 받을 확률이 3배 높아요!"
-      />
-      <SubTitle text="전문 분야 (복수 선택 가능)" />
-      <Selector
-        multiple
-        options={choices}
-        multiSelected={selected}
-        setMultiSelected={setSelected}
-      />
-      <SubTitle text="커리큘럼" />
-      <TextInput
-        mode="outlined"
-        placeholder="코치님만의 수업 방식을 자유롭게 작성해주세요! (최대 1000자)"
-        value={curriculum}
-        onChangeText={(text) => setCurriculum(text)}
-        textColor="black"
-        placeholderTextColor="gray"
-        multiline
-        maxLength={1000}
-        style={{ height: 150 }}
-        testID="curriculum"
-      />
-      <SubTitle text="경력/자격" />
-      <TextInput
-        mode="outlined"
-        placeholder={"예)\n- 커리어1\n- 커리어2\n- 커리어3\n- ..."}
-        value={career}
-        onChangeText={(text) => setCareer(text)}
-        textColor="black"
-        placeholderTextColor="gray"
-        multiline
-        maxLength={1000}
-        style={{ height: 150 }}
-        testID="career"
-      />
-      <Divider />
-      <Text variant="titleLarge" style={styles.title}>
-        소개/레슨 영상
-      </Text>
-      <Text variant="titleMedium" style={styles.subtitle}>
-        소개 영상
-      </Text>
-      <Button
-        mode="contained"
-        onPress={handleSubmitSuccess}
-        style={styles.button}
+    <>
+      <ScrollView
+        style={styles.container}
+        automaticallyAdjustKeyboardInsets
+        keyboardDismissMode="on-drag"
       >
-        다음 (1/3)
-      </Button>
-    </ScrollView>
+        <MainTitle
+          text="상세 정보"
+          sub="상세 정보를 다 입력하면 문의 받을 확률이 3배 높아요!"
+        />
+        <SubTitle text="전문 파트 (복수 선택 가능)" />
+        <Selector
+          multiple
+          options={partChoices}
+          multiSelected={selectedParts}
+          setMultiSelected={setSelectedParts}
+        />
+        <SubTitle text="레슨 레벨 (복수 선택 가능)" />
+        <Selector
+          multiple
+          options={levelChoices}
+          multiSelected={selectedLevels}
+          setMultiSelected={setSelectedLevels}
+        />
+        <Divider style={{ marginVertical: 10 }} />
+        <SubTitle
+          text="소개글"
+          sub=" 코치님의 소개글을 자유롭게 작성해주세요. (최대 1000자)"
+        />
+        <TextInput
+          mode="outlined"
+          placeholder={
+            "선수 경력: \n- 양천중-서울고-서울대\n- 2018: 두산 베어스 입단\n\n코치 경력: \n- 2023년 키움 히어로즈 작전코치\n\n소개글:\n안녕하세요 홍승우입니다.\n\n야구 입문자부터 선수까지 개개인의 특성에 맞춰 친절하고 자세히 설명해드립니다.\n\n비디오 분석을 통한 꼼꼼한 레슨으로 여러분들의 야구 실력을 향상시켜드리겠습니다."
+          }
+          value={intro}
+          onChangeText={(text) => setIntro(text)}
+          textColor="black"
+          placeholderTextColor="gray"
+          multiline
+          maxLength={1000}
+          style={{ height: 300 }}
+          testID="curriculum"
+        />
+        <Divider style={{ marginVertical: 10 }} />
+        <SubTitle text="소개 사진/영상" sub=" 소개 영상은 최대 15MB" />
+        <ImagePicker uploadedImages={medias} setUploadedImages={setMedias} />
+        <Divider style={{ marginVertical: 10 }} />
+        <SubTitle
+          text="선호 활동 지역"
+          sub=" 최소 1개 최대 5개"
+        />
+        {selectedRegions.length > 0 && (
+          <Text style={{ marginBottom: 10 }}>
+            선택한 지역: {selectedRegions.map((region) => region.name).join(", ")}
+          </Text>
+        )}
+        <Button onPress={() => bottomSheetRef.current?.expand()}>
+          {selectedRegions.length > 0 ? "다시 선택하기" : "선택하기"}
+        </Button>
+        <Button
+          mode="contained"
+          onPress={handleSubmitSuccess}
+          style={styles.button}
+        >
+          다음 (1/3)
+        </Button>
+      </ScrollView>
+      <BottomSheet
+        ref={bottomSheetRef}
+        index={-1}
+        snapPoints={snapPoints}
+        enableHandlePanningGesture={false}
+      >
+        <ScrollView style={{ flex: 1, paddingHorizontal: 10 }}>
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ flex: 2, marginRight: 10 }}>시/도</Text>
+            <View style={{ flex: 8 }}>
+              <Selector
+                options={data?.sido.map((sido) => sido.label) || []}
+                singleSelected={selectedSido}
+                setSingleSelected={setSelectedSido}
+                noIcon
+              />
+            </View>
+          </View>
+          <Divider style={{ marginVertical: 10 }} />
+          <View style={{ flexDirection: "row" }}>
+            <Text style={{ flex: 2, marginRight: 10 }}>시/군/구</Text>
+            <View style={{ flex: 8 }}>
+              <RegionSelector
+                options={data?.sigungu[selectedSido] || []}
+                multiSelected={selectedRegions}
+                setMultiSelected={setSelectedRegions}
+                showSnackBar={setVisible}
+              />
+            </View>
+          </View>
+        </ScrollView>
+        {selectedRegions.length > 0 && (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              flexWrap: "wrap",
+            }}
+          >
+            {selectedRegions.map((region) => (
+              <TouchableOpacity
+                key={region.code}
+                onPress={() => removeSelected(region)}
+              >
+                <Chip style={{ margin: 5, backgroundColor: "green" }}>
+                  {region.name}
+                </Chip>
+              </TouchableOpacity>
+            ))}
+          </View>
+        )}
+        <Button
+          mode="contained"
+          onPress={() => bottomSheetRef.current?.close()}
+          style={styles.button}
+        >
+          확인
+        </Button>
+        <Snackbar
+          visible={visible}
+          onDismiss={() => setVisible(false)}
+          action={{
+            label: "확인",
+            onPress: () => setVisible(false),
+          }}
+        >
+          최대 5개까지 선택할 수 있어요!
+        </Snackbar>
+      </BottomSheet>
+    </>
   );
 }
 
