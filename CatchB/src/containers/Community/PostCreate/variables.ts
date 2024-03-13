9bcb7d9e973310f824e30e1c1dbe00280da3a075
vietman2 <vietman0@gmail.com>
2024-03-13 11:50:35 +0900

    redefine myimageasset

--- CatchB/src/containers/Community/PostCreate/variables.ts
+++ CatchB/src/containers/Community/PostCreate/variables.ts
@@ -1,11 +1,9 @@
-import { ImagePickerAsset } from "expo-image-picker";
-
 const forumChoices: string[] = ["덕아웃", "드래프트", "장터"];
 
 type MyImageAsset = {
   id: number;
   url: string;
-  asset: ImagePickerAsset;
+  fileName: string;
 };
 
 export { forumChoices, MyImageAsset };
