ffb20bee01ec1e5bcae22c8ba1c67e37bcac0a9e
vietman2 <vietman0@gmail.com>
2024-03-20 10:28:48 +0900

    add refresh

--- CatchB/src/containers/Base/ErrorPage.tsx
+++ CatchB/src/containers/Base/ErrorPage.tsx
@@ -1,11 +1,19 @@
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Text } from "react-native-paper";
 
-export default function ErrorPage() {
+interface Props {
+  onRefresh?: () => void;
+}
+
+export default function ErrorPage({ onRefresh }: Props) {
   return (
     <View style={styles.container}>
       {/* TODO: 아이콘이나 이미지 추가하기 */}
       <Text variant="headlineSmall">오류가 발생했습니다.</Text>
+      <Text variant="titleMedium">잠시 후 다시 시도해주세요.</Text>
+      <TouchableOpacity onPress={onRefresh} style={styles.button}>
+        <Text variant="titleMedium">새로고침</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -15,5 +23,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+  button: {
+    marginTop: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 10,
+    backgroundColor: "lightgray",
   },
 });
