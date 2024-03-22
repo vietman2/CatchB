2404c82f83d82c57da5bac563e8ce1bf3c5b23ee
vietman2 <vietman0@gmail.com>
2024-03-22 17:32:02 +0900

    refactor scrollview and implement refresh

---
+++ CatchB/src/components/ScrollView/ScrollWithRefresh.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import { RefreshControl, ScrollView } from "react-native";
+
+interface Props {
+  children: React.ReactNode;
+  refreshing: boolean;
+  onRefresh: () => void;
+}
+
+export default function ScrollWithRefresh({ children, refreshing, onRefresh }: Props) {
+  const [isPulledDown, setIsPulledDown] = useState<boolean>(false);
+
+  const onScroll = (event: any) => {
+    const scrollY = event.nativeEvent.contentOffset.y;
+    if (scrollY < -100) {
+      setIsPulledDown(true);
+    } else {
+      setIsPulledDown(false);
+    }
+  };
+
+  const onScrollEndDrag = () => {
+    if (isPulledDown && !refreshing) {
+      onRefresh();
+    }
+  };
+
+  return (
+    <ScrollView
+      refreshControl={<RefreshControl refreshing={refreshing} />}
+      onScroll={onScroll}
+      onScrollEndDrag={onScrollEndDrag}
+      scrollEventThrottle={16}
+    >
+      {children}
+    </ScrollView>
+  );
+}
---
+++ CatchB/src/components/ScrollView/index.tsx
@@ -0,0 +1,3 @@
+import { default as ScrollView } from "./ScrollWithRefresh";
+
+export { ScrollView };
