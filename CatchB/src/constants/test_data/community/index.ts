2c1ce6b5ab16e4f6cb2b08543d8ac12dc6a17d4f
vietman2 <vietman0@gmail.com>
2024-03-25 12:24:57 +0900

    create test data

--- CatchB/src/constants/test_data/community/index.ts
+++ CatchB/src/constants/test_data/community/index.ts
@@ -1,4 +1,5 @@
+import { sampleComments } from "./comments";
 import { tag1, sampleTags } from "./tags";
 import { samplePosts, sampleSimplePosts } from "./posts";
 
-export { tag1, sampleTags, samplePosts, sampleSimplePosts };
+export { tag1, sampleComments, sampleTags, samplePosts, sampleSimplePosts };
