617de44a4ea8991a1ee41526c3b6b2b917801a7a
vietman2 <vietman0@gmail.com>
2024-03-25 17:18:25 +0900

    create community button component

--- CatchB/src/components/Buttons/Buttons.test.tsx
+++ CatchB/src/components/Buttons/Buttons.test.tsx
@@ -1,8 +1,9 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { render } from "@testing-library/react-native";
+import { fireEvent, render } from "@testing-library/react-native";
 
 import {
   BackButton,
+  CommunityButton,
   IconTextButton,
   NaverButton,
   KakaoButton,
@@ -43,5 +44,31 @@ describe("<IconButton />", () => {
 describe("<TabButton />", () => {
   it("renders correctly", () => {
     render(<TabButton title="Home" detail="Detail" showArrow />);
+  });
+});
+
+describe("<CommunityButton />", () => {
+  it("renders active like button correctly and handles press", () => {
+    const { getByTestId } = render(
+      <CommunityButton mode="like" state action={() => {}} />
+    );
+
+    fireEvent.press(getByTestId("community-button"));
+  });
+
+  it("renders inactive like button correctly", () => {
+    render(<CommunityButton mode="like" number={10} />);
+  });
+
+  it("renders active like button correctly", () => {
+    render(<CommunityButton mode="dislike" state />);
+  });
+
+  it("renders inactive dislike button correctly", () => {
+    render(<CommunityButton mode="dislike" />);
+  });
+
+  it("renders report button correctly", () => {
+    render(<CommunityButton mode="report" />);
   });
 });
