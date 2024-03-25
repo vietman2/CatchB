b91f249c1843135952b20c6bfad05a6a70e5442f
vietman2 <vietman0@gmail.com>
2024-03-25 12:21:40 +0900

    add comment functions

--- CatchB/src/containers/Community/PostDetail/PostDetail.tsx
+++ CatchB/src/containers/Community/PostDetail/PostDetail.tsx
@@ -1,30 +1,51 @@
 import { useRef, useMemo, useState, useEffect } from "react";
 import { ScrollView, StyleSheet, View } from "react-native";
-import { Divider, Text } from "react-native-paper";
+import { Divider, Text, TextInput } from "react-native-paper";
 import { useSelector } from "react-redux";
 import BottomSheet from "@gorhom/bottom-sheet";
 
+import { CommentSimple, PostHeader } from "../fragments";
 import { ErrorPage } from ".components/Error";
 import { LoadingPage } from ".components/Loading";
-import { CommunityProfile } from ".components/Profile";
-import { getPostDetail } from ".services/community";
+import { createComment, getPostDetail } from ".services/community";
 import { RootState } from ".store/index";
 import { themeColors } from ".themes/colors";
 import { PostDetailType } from ".types/community";
 
 export default function PostDetail() {
+  const [newComment, setNewComment] = useState<string>("");
+
   const bottomSheetRef = useRef<BottomSheet>(null);
   const snapPoints = useMemo(() => ["10%", "50%", "100%"], []);
   const [post, setPost] = useState<PostDetailType>();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const user = useSelector((state: RootState) => state.auth.user);
+  const token = useSelector((state: RootState) => state.auth.token);
   const postId = useSelector(
     (state: RootState) => state.community.selectedPostId
   );
 
+  const handleComment = async () => {
+    const response = await createComment(postId, newComment, user.uuid, token);
+
+    if (response.status === 201) {
+      setPost((prevPost) => {
+        if (prevPost) {
+          return {
+            ...prevPost,
+            num_comments: prevPost.num_comments + 1,
+          };
+        }
+        return prevPost;
+      });
+      setNewComment("");
+    }
+  };
+
   useEffect(() => {
     const fetchPost = async () => {
-      const response = await getPostDetail(postId);
+      const response = await getPostDetail(postId, token);
 
       if (response.status === 200) {
         setPost(response.data);
@@ -47,17 +68,17 @@ export default function PostDetail() {
         <Text variant="headlineSmall" style={styles.title}>
           {post.title}
         </Text>
-        <CommunityProfile post={post} />
-        <Divider />
+        <PostHeader post={post} />
+        <Divider style={styles.divider} />
         <Text variant="titleMedium" style={styles.body}>
           {post.content}
         </Text>
-        <Divider />
+        <Divider style={styles.divider} />
         <View style={styles.interactions}>
           <Text variant="titleSmall">공감하기</Text>
           <Text variant="titleSmall">신고하기</Text>
         </View>
-        <Divider />
+        <Divider style={styles.divider} />
       </ScrollView>
       <BottomSheet
         ref={bottomSheetRef}
@@ -66,10 +87,25 @@ export default function PostDetail() {
         backgroundStyle={styles.sheetBackground}
       >
         <View style={styles.comments}>
-          <Text variant="titleMedium" style={styles.commentTitle}>
-            댓글 {post.num_comments}
-          </Text>
-          <Divider />
+          <Text variant="titleMedium">댓글 {post.num_comments}</Text>
+          <Divider style={styles.divider} />
+          <TextInput
+            value={newComment}
+            onChangeText={setNewComment}
+            placeholder="댓글 쓰기"
+            mode="outlined"
+            outlineColor="transparent"
+            dense
+            right={
+              newComment ? (
+                <TextInput.Icon icon="send" onPress={handleComment} />
+              ) : null
+            }
+          />
+          <Divider style={styles.divider} />
+          {post.comments.map((comment) => (
+            <CommentSimple key={comment.id} comment={comment} />
+          ))}
         </View>
       </BottomSheet>
     </>
@@ -100,10 +136,10 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     marginVertical: 10,
   },
-  commentTitle: {
-    marginBottom: 10,
-  },
   sheetBackground: {
     backgroundColor: "rgb(250, 250, 250)",
+  },
+  divider: {
+    marginVertical: 5,
   },
 });
