9dd3e781cd43d1bfce419e1145a1c615f117ec9c
vietman2 <vietman0@gmail.com>
2024-03-27 16:04:19 +0900

    add reason choices

--- CatchB/src/constants/enums/community.ts
+++ CatchB/src/constants/enums/community.ts
@@ -1,3 +1,17 @@
 const forumChoices: string[] = ["덕아웃", "드래프트", "장터"];
 
-export { forumChoices };
+const reasonChoices = [
+  "욕설/비방",
+  "불쾌한 컨텐츠",
+  "음란물",
+  "허위정보",
+  "도배/스팸/광고",
+  "폭력/협박/위협",
+  "개인정보 침해",
+  "정치적인 내용",
+  "잘못된 게시판/태그",
+  "기타",
+];
+
+
+export { forumChoices, reasonChoices };
